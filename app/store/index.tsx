7d81ce6f20b21e2b786014e639ebb5d3ffd0e1d1
thomas <thomasmartuaps@gmail.com>
2026-04-16 11:19:46 +0700

    index store

--- app/store/index.tsx
+++ app/store/index.tsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { rootReducer } from "./reducers";
 import createSagaMiddleware from "redux-saga";
-import rootSaga from "./saga/rootSage";
+import rootSaga from "./saga/rootSaga";
 
 const saga = createSagaMiddleware();
 
